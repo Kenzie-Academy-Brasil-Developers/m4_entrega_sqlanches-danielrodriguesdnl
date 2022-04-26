863adc7bef2006de06d51561d5f4400e040ddf4c
Daniel Rodrigues <dnll.srodrigues@gmail.com>
2022-04-26 15:38:24 -0300

    projeto finalizado

--- crud.sql
+++ crud.sql
@@ -5,19 +5,44 @@
 
 -- 1)
 
-
+INSERT INTO clientes
+          (nome, lealdade)
+        VALUES
+          ('Georgia', 0);
 
 -- 2)
 
+INSERT INTO pedidos 
+            (status, cliente_id)
+        VALUES
+          ('Recebido', 6);
 
 
 -- 3)
 
-
+INSERT INTO produtos_pedidos
+          (pedido_id, produto_id)
+        VALUES
+          (6, 1),
+          (6, 2),
+          (6, 6),
+          (6, 8),
+          (6, 8);
 
 -- Leitura
 
 -- 1)
+
+SELECT 
+	c.*,
+	o.*,
+    p.*
+FROM
+		pedidos o
+        JOIN produtos_pedidos pp ON o.id = pp.pedido_id
+        JOIN produtos p ON pp.produto_id = p.id
+        JOIN clientes c ON o.cliente_id = c.id
+        WHERE c.id = (SELECT id FROM clientes WHERE nome = 'Georgia');
 
 
 
--- selects.sql
+++ selects.sql
@@ -3,10 +3,58 @@
 
 -- 1)
 
+SELECT 
+	o.*,
+    p.*
+
+FROM
+		pedidos o
+        JOIN produtos_pedidos pp ON o.id = pp.pedido_id
+        JOIN produtos p ON pp.produto_id = p.id;
+
+
 -- 2)
+
+SELECT 
+	o.id
+FROM
+		pedidos o
+        JOIN produtos_pedidos pp ON o.id = pp.pedido_id
+        JOIN produtos p ON pp.produto_id = p.id
+        WHERE p.id = (SELECT id FROM produtos WHERE nome = 'Fritas');
 
 -- 3)
 
+       
+SELECT 
+	c.nome gostam_de_fritas
+FROM
+		clientes c
+        JOIN pedidos o ON C.id = o.id
+        JOIN produtos_pedidos pp ON o.id = pp.pedido_id
+        JOIN produtos p ON pp.produto_id = p.id
+        WHERE p.id = (SELECT id FROM produtos WHERE nome = 'Fritas');
+
 -- 4)
 
+SELECT 
+	 SUM(p.preço) sum
+FROM
+		produtos p
+        JOIN produtos_pedidos pp ON p.id = pp.produto_id
+        JOIN pedidos o ON pp.pedido_id = o.id
+        JOIN clientes c ON o.cliente_id = c.id
+        WHERE c.id = (SELECT id FROM clientes WHERE nome = 'Laura');
+        
+
 -- 5)
+
+SELECT
+	p.nome nome,
+    COUNT(p.nome) count
+FROM
+		produtos p
+        JOIN produtos_pedidos pp ON p.id = pp.produto_id
+        JOIN pedidos o ON pp.pedido_id = o.id
+        GROUP BY nome ORDER BY nome ASC;
+       
